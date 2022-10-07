0cf9ff65eef1ab37abd1977f9891bc8224d210a2
Radli1 <i6100009@gmail.com>
2022-10-07 13:42:23 +0400

    modified

---
+++ pages/ArticleList1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <PaginationComponent />
+      <SortComponent />
+      <!-- <div v-for="index in paginatedArray" :key="index">
+      <h2>{{paginatedArray.id}}</h2>
+      <p>{{paginatedArray.title}}</p>
+    </div> -->
+    </div>
+  </div>
+</template>
+<script>
+import PaginationComponent from '../src/components/PaginationComponent.vue';
+import SortComponent from '../src/components/SortComponent.vue';
+
+export default {
+  components: {
+    PaginationComponent,
+    SortComponent,
+  },
+};
+</script>
+
+<!-- <script setup>
+  //import { ref, computed, reactive } from "@nuxtjs/composition-api";
+  import  PaginationComponent from './src/components';
+  import  SortComponent from '@/src/components'
+
+  const { paginatedArray } = storeToRefs(useTitleStore())
+  
+  defineProps(['paginatedArray']) -->
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Personal Diary</h1>
+</template>
+
+<script>
+export default {
+  head() {
+    // Set Meta Tags for this Page
+  },
+  // ...
+};
+</script>
+
+<style>
+h1 {
+  font-size: large;
+  color: blue;
+  text-align: center;
+}
+</style>
