e045c10cd86bd13ed0211ac9e89ea95b2aee0c40
Radli1 <i6100009@gmail.com>
2022-10-07 14:30:05 +0400

    modified

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
     <div>
-      <AppPagination />
-    </div>
-   <div>
-      <AppSort />
+      <index.vue />
+      <article-list />
+      <diary-page />
+      <new-article /> 
    </div>
   </div>
-
-
-
-
 </template>
-
 <script>
+import DiaryPage from '@/DiaryPage.vue';
+import NewArticle from '@/NewArticle.vue';
+import ArticleList from '@/ArticleList.vue';
+import ArticleList from '@/ArticleList.vue';
+
+export default {
+  components: {
+    ArticleList ,
+    NewArticle ,
+    DiaryPage,
+    ArticleList 
+  }
+}
 
 
 
