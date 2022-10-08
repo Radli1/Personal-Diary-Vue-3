eb3abbccb75a63b396f7d0cf950faa47e427380b
Radli1 <i6100009@gmail.com>
2022-10-08 13:51:02 +0400

    modified

---
+++ Personal Diary Vue 3/src/components/AppSort.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="orderType='asc'">Сортировка по возрастанию</button>
+  <button @click="orderType='desc'">Сортировка по убыванию</button>
+
+</template>
+<script>
+export default {
+    setup() {
+        if (orderType='asc') {
+          console.log('true statement');
+          paginatedArray.sort((a, b) => {
+            if (orderType === 'asc') {
+              return b.id - a.id;
+            }
+            return a.id - b.id;
+          })
+      }
+      return {
+          searchTitle,
+          result,
+          listTitles
+          }
+      }
+    }
+  </script>
+
+  <template>
+     <button class="sort" @click="orderType">Сортировка по возрастанию
+   </button>
+  </template>
---
+++ Personal Diary Vue 3/src/views/ArticleList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <PaginationComponent />
+      <SortComponent />
+      <!-- <div v-for="index in paginatedArray" :key="index">
+      <h2>{{paginatedArray.id}}</h2>
+      <p>{{paginatedArray.title}}</p>
+    </div> -->
+    </div>
+  </div>
+</template>
+<script>
+import AppPagination from '../components/PaginationComponent.vue';
+import AppSort from '../components/SortComponent.vue';
+
+export default {
+  components: {
+    AppPagination,
+    AppSort,
+  },
+};
+</script>
+
+<!-- <script setup>
+  //import { ref, computed, reactive } from "@nuxtjs/composition-api";
+  import  PaginationComponent from './src/components';
+  import  SortComponent from '@/src/components'
+
+  const { paginatedArray } = storeToRefs(useTitleStore())
+  
+  defineProps(['paginatedArray']) -->
